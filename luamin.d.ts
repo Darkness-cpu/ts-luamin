c387c8c1dabf1b95c72fe81278f1c70740bb33aa
Darkness-cpu <mistakessadly@gmail.com>
2025-01-18 14:56:10 +0700

    Update luamin.d.ts

--- luamin.d.ts
+++ luamin.d.ts
@@ -1,1 +1,22 @@
+declare module "lunmin" {
+  // ตัวอย่างการกำหนด type
+  export interface LuaParseOptions {
+    comments: boolean;
+    scope: boolean;
+  }
 
+  export interface LuaParse {
+    defaultOptions: LuaParseOptions;
+    parse(code: string): any;
+  }
+
+  export function generateIdentifier(originalName: string): string;
+
+  export function formatBase(base: any): string;
+
+  export function formatExpression(expression: any, options?: { precedence?: number; preserveIdentifiers?: boolean }): string;
+
+  export function formatArguments(argumentsList: any[]): string;
+
+  export function formatFunction(func: any): string;
+}
