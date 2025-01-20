d1889543c92e11ec4b901da3afaaff10e15b0832
Darkness-cpu <mistakessadly@gmail.com>
2025-01-20 11:47:41 +0700

    Update luamin.d.ts

--- luamin.d.ts
+++ luamin.d.ts
@@ -1,22 +1,56 @@
-declare module "lunmin" {
-  // ตัวอย่างการกำหนด type
-  export interface LuaParseOptions {
-    comments: boolean;
-    scope: boolean;
+declare module "luamin" {
+  export type LuaparseOptions = {
+    comments?: boolean;
+    scope?: boolean;
+  };
+
+  export interface Luaparse {
+    parse: (code: string, options?: LuaparseOptions) => any;
+    defaultOptions: LuaparseOptions;
   }
 
-  export interface LuaParse {
-    defaultOptions: LuaParseOptions;
+  export const PRECEDENCE: Record<string, number>;
+
+  export const IDENTIFIER_PARTS: string[];
+  export const IDENTIFIER_PARTS_MAX: number;
+
+  export const regexAlphaUnderscore: RegExp;
+  export const regexAlphaNumUnderscore: RegExp;
+  export const regexDigits: RegExp;
+
+  export const each: <T>(
+    array: T[],
+    fn: (item: T, isLast: boolean) => void
+  ) => void;
+
+  export const indexOf: <T>(array: T[], value: T) => number;
+
+  export const extend: <T, U>(destination: T, source: U) => T & U;
+
+  export const generateZeroes: (length: number) => string;
+
+  export const isKeyword: (id: string) => boolean;
+
+  export const generateIdentifier: (originalName: string) => string;
+
+  export const joinStatements: (
+    a: string,
+    b: string,
+    separator?: string
+  ) => string;
+
+  export const formatBase: (base: any) => string;
+
+  export const formatExpression: (
+    expression: any,
+    options?: {
+      precedence?: number;
+      preserveIdentifiers?: boolean;
+    }
+  ) => string;
+
+  export interface Luamin {
     parse(code: string): any;
+    formatExpression(expression: any): string;
   }
-
-  export function generateIdentifier(originalName: string): string;
-
-  export function formatBase(base: any): string;
-
-  export function formatExpression(expression: any, options?: { precedence?: number; preserveIdentifiers?: boolean }): string;
-
-  export function formatArguments(argumentsList: any[]): string;
-
-  export function formatFunction(func: any): string;
 }
